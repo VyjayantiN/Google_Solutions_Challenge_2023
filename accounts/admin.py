ed290158eff6c6cb24ed3b39748b8093f140620d
Ridhima Thakur <ridhimathakur833@gmail.com>
2023-03-27 21:52:19 +0530

    commit

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-
+from .models import recipe,gen_ins
 # Register your models here.
 """
 from .models import Person
 admin.site.register(Person)
 """
+admin.site.register(recipe)
+admin.site.register(gen_ins)
